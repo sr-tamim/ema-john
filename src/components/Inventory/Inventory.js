d90fc1ed03afca71451364b560a8aba560d76772
SR TAMIM <saifur.tamim@gmail.com>
2022-03-25 07:17:23 +0600

    inventory page added

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,10 @@
+
+const Inventory = () => {
+    return (
+        <div>
+            <h1 style={{ textAlign: 'center' }}>Authorised Persons Only</h1>
+        </div>
+    );
+};
+
+export default Inventory;
