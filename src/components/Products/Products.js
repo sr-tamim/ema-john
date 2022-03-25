8c25cfd1b59be1baa133d08d2fa8b11e5557b4c4
SR TAMIM <saifur.tamim@gmail.com>
2022-03-25 07:17:10 +0600

    pagination integrated

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ProductsContext } from '../../contexts/ProductsContext';
 import Product from '../Product/Product';
 import './Products.css';
+import Search from "../Search/Search";
 
-const Products = (props) => {
+const Products = () => {
+    const { products, page, setPage, totalPage } = useContext(ProductsContext)
     return (
-        <div id="products">
-            <h2>Products</h2>
-            {
-                props.showProducts.map(product => <Product key={product.key} product={product} addToCart={props.addToCart} />)
-            }
-        </div>
+        <>
+            <Search />
+            <div id="products">
+                <h2>Products</h2>
+                {
+                    products.map(product => <Product key={product._id} product={product} />)
+                }
+            </div>
+            <div className="pagination">
+                {
+                    [...Array(totalPage).keys()].map(i =>
+                        <button key={i} onClick={() => setPage(i)}
+                            className={page === i ? 'active' : ''}
+                        >{i + 1}</button>)
+                }
+            </div>
+        </>
     );
 
 };
