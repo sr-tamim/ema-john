95cda6bb9d0708f5acf4a76cd90037e91235bee1
SR TAMIM <saifur.tamim@gmail.com>
2022-03-25 07:16:35 +0600

    navbar updated with new links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../Header/Header.css'
 import logo from '../../images/logo.png';
+import { NavLink } from 'react-router-dom';
+import { ProductsContext } from '../../contexts/ProductsContext';
+import { UserContext } from '../../contexts/UserContext';
+
+const showHideCart = () => {
+    document.getElementById('cart').classList.toggle('hide');
+}
 
 const Header = () => {
+    const { user } = useContext(UserContext);
+    const { cart } = useContext(ProductsContext);
+    const cartItems = cart.length;
+
     return (
         <header>
             <img src={logo} alt="logo" />
             <nav>
-                <a href="/shop">Shop</a>
-                <a href="/review">Order Review</a>
-                <a href="/inventory">Manage Inventory here</a>
+                <NavLink activeClassName="active" to="/shop">Shop</NavLink>
+                <NavLink activeClassName="active" to="/review">Order Review</NavLink>
+                <NavLink activeClassName="active" to="/inventory">Inventory</NavLink>
+                {user ? <NavLink activeClassName="active" to="/profile">Profile</NavLink>
+                    : <NavLink activeClassName="active" to="/login">Login</NavLink>}
+                <span id="cart-icon">
+                    <i className="fas fa-shopping-cart" onClick={showHideCart} ></i>
+                    {cartItems > 0 ? <small id="cart-items">{cartItems}</small> : ''}
+                </span>
             </nav>
         </header>
     );
